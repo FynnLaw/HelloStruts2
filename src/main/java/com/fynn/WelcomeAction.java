2987bf193a5c3f5c81be0fdb862bf5b148fb5a2f
FynnLaw <fynnlaw@gmail.com>
2016-07-08 16:38:41 +0800

    first commit

---
+++ src/main/java/com/fynn/WelcomeAction.java
@@ -0,0 +1,18 @@
+package com.fynn;
+
+public class WelcomeAction{
+     private String username;
+     
+     public String getUsername() {
+         return username;
+     }
+     
+     public void setUsername(String username) {
+         this .username = username;
+     }
+   
+     // all struts logic here
+     public  String execute() {
+         return "SUCCESS" ;
+     }
+}
